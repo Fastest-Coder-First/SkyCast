25f3a30c15ee2cacba010e4477a32e8e1dd0a165
Devendra Parihar <54232149+Devparihar5@users.noreply.github.com>
2023-06-24 17:50:33 +0530

    Update geolocation.py

--- get_geolocation/geolocation.py
+++ get_geolocation/geolocation.py
@@ -1,20 +1,8 @@
-import geocoder
 from geopy.geocoders import Nominatim
 
 class GeolocationModel:
     def __init__(self):
         self.geolocator = Nominatim(user_agent="my-app")
-
-    @staticmethod
-    def get_current_location():
-        # Use geocoder to get the current location
-        g = geocoder.ip('me')
-        if g.latlng:
-            latitude, longitude = g.latlng
-            return latitude, longitude
-        else:
-            return None
-
     def get_location_by_name(self, location):
         location_data = self.geolocator.geocode(location)
 
